0ba13b61419bc681c86189f7d3e81c81246541ab
Jeffrey Stines <jmstines00@gmail.com>
2025-11-22 18:27:06 -0500

    Add CORS mode to fetch request in submitContactForm function

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -44,6 +44,7 @@ export default function Contact() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
+      mode: 'cors',
     });
 
     if (!response.ok) {
